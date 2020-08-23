109478653212319159a163832f0e4ce314d166e4
Servo97 <sbpatil97@gmail.com>
2020-08-23 18:25:07 +0530

    Pokedex

---
+++ README.md
@@ -0,0 +1,4 @@
+# PokeDex Android App
+
+Basic starter app from Stanford CS193a course (2017)
+Code to create a pokedex of 12 pokemon!
---
+++ app/src/main/java/edu/stanford/cs193a/pokedex/PokeActivity.java
@@ -0,0 +1,17 @@
+package edu.stanford.cs193a.pokedex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poke);
+    }
+
+    public void pokemonClick(View view) {
+    }
+}
